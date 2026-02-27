899eb0eaf159be018afd7edf95bb0ed874a2461a
Danillo <danilo_victor2004@hotmail.com>
2026-02-26 22:21:45 -0300

    feat:  Connecting to a PostgreSQL database, creating a users model, and inserting a record into the users table.

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,17 +1,21 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { prisma } from "@/prisma";
 
 class UsersController {
   async index(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 
   async create(request: Request, response: Response) {
-    return response.status(201).json()
+    const { name, email } = request.body;
+
+    await prisma.user.create({ data: { name, email } });
+    return response.status(201).json();
   }
 
   async show(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 }
 
-export { UsersController }
+export { UsersController };
